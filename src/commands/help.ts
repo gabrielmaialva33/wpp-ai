d1f4c1dd6f1f42dd1376ebd4a92260f0616ccc28
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-07-09 23:41:07 -0300

    feat: implement help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -4,13 +4,13 @@ import { getCommandsList } from '../bot.js'
 
 export const help = {
   name: 'help',
-  description: 'list comandos disponíveis',
+  description: 'comandos disponíveis',
   execute: async (client: Whatsapp, message: Message) => {
     const commandsList = getCommandsList()
-    let response = 'Lista de comandos disponíveis:\n\n'
+    let response = '*Comandos disponíveis*:\n\n'
 
     commandsList.forEach((command) => {
-      response += `*${command.name}*: ${command.description}\n`
+      response += `\`${command.name}\`: \`\`\`${command.description}\`\`\`\n\n`
     })
 
     await client.sendText(message.from, response)
